4e6c00faf8ef8edad806a512475aa8dfe574536c
root <benny.taccardi@gmail.com>
2018-07-17 23:07:27 +0200

    first commit

---
+++ src/Entity/Product.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Product
+ *
+ * @ORM\Table(name="Product")
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="idProduct", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idproduct;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="productName", type="string", length=100, nullable=false)
+     */
+    private $productname;
+
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="Purchased", type="boolean", nullable=true)
+     */
+    private $purchased;
+
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * @ORM\ManyToMany(targetEntity="User", mappedBy="productproduct")
+     */
+    private $useruser;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->useruser = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+}
+
