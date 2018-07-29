61fadca148a2749cdeab1f0b24965112e7d8dd7e
benny <benny.taccardi@gmail.com>
2018-07-29 12:32:43 +0200

    vhcjv

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -8,7 +8,6 @@ use Doctrine\ORM\Mapping as ORM;
  * Product
  *
  * @ORM\Table(name="product")
- * @ORM\Entity
  * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
  */
 class Product
