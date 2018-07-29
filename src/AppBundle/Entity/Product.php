c60eda4223d4e9a4d8b144cbb55d0fc408e51754
benny <benny.taccardi@gmail.com>
2018-07-29 12:13:25 +0200

    vhcjv

--- src/AppBundle/Entity/Product.php
+++ src/AppBundle/Entity/Product.php
@@ -8,6 +8,7 @@ use Doctrine\ORM\Mapping as ORM;
  * Product
  *
  * @ORM\Table(name="product")
+ * @ORM\Entity
  * @ORM\Entity(repositoryClass="AppBundle\Repository\ProductRepository")
  */
 class Product
@@ -36,6 +37,13 @@ class Product
     private $purchased;
 
     /**
+     * @var string
+     *
+     * @ORM\Column(name="path", type="string", length=500, nullable=false)
+     */
+    private $path;
+
+    /**
      * @var \Doctrine\Common\Collections\Collection
      *
      * @ORM\ManyToMany(targetEntity="User", mappedBy="productproduct")
@@ -50,96 +58,5 @@ class Product
         $this->useruser = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
-
-    /**
-     * Get idproduct
-     *
-     * @return integer
-     */
-    public function getIdproduct()
-    {
-        return $this->idproduct;
-    }
-
-    /**
-     * Set productname
-     *
-     * @param string $productname
-     *
-     * @return Product
-     */
-    public function setProductname($productname)
-    {
-        $this->productname = $productname;
-
-        return $this;
-    }
-
-    /**
-     * Get productname
-     *
-     * @return string
-     */
-    public function getProductname()
-    {
-        return $this->productname;
-    }
-
-    /**
-     * Set purchased
-     *
-     * @param boolean $purchased
-     *
-     * @return Product
-     */
-    public function setPurchased($purchased)
-    {
-        $this->purchased = $purchased;
-
-        return $this;
-    }
-
-    /**
-     * Get purchased
-     *
-     * @return boolean
-     */
-    public function getPurchased()
-    {
-        return $this->purchased;
-    }
-
-    /**
-     * Add useruser
-     *
-     * @param \AppBundle\Entity\User $useruser
-     *
-     * @return Product
-     */
-    public function addUseruser(\AppBundle\Entity\User $useruser)
-    {
-        $this->useruser[] = $useruser;
-
-        return $this;
-    }
-
-    /**
-     * Remove useruser
-     *
-     * @param \AppBundle\Entity\User $useruser
-     */
-    public function removeUseruser(\AppBundle\Entity\User $useruser)
-    {
-        $this->useruser->removeElement($useruser);
-    }
-
-    /**
-     * Get useruser
-     *
-     * @return \Doctrine\Common\Collections\Collection
-     */
-    public function getUseruser()
-    {
-        return $this->useruser;
-    }
 }
+
